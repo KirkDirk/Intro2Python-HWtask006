912ac1a50c048080eea8875f5791b1784e799d89
Kirk Dirk <jackalex@ya.ru>
2022-09-29 23:35:05 +0500

    add code

--- HWtask006.py
+++ HWtask006.py
@@ -1,5 +1,12 @@
-# Напишите программу, которая принимает на вход 
+# Напишите программу, которая принимает на вход
 # вещественное число и показывает сумму его цифр.
 # Пример:
 # - 6782 -> 23
 # - 0,56 -> 11
+
+numfloat = str(input('Введите вещественное число: '))
+sumdigit = 0
+for i in range(len(numfloat)):
+    if numfloat[i] != '.' and numfloat[i] != ',':
+        sumdigit += int(numfloat[i])
+print(sumdigit)
